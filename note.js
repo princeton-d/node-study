79037b565e2971f80fda0e6f6dee6a324d918dba
websteak <ssk0080@gmail.com>
2023-01-06 22:00:29 +0900

    get 메소드

--- note.js
+++ note.js
@@ -13,3 +13,24 @@ function 패키지_삭제() { // 패키지 삭제
   console.log(`
 다운로드 했던 패키지를 삭제하려면 $ npm uninstall (패키지 이름)을 입력해주면 간단하게 삭제할 수 있다.`)
 }
+
+function get_메소드() { // get 메소드
+  const fs = require('express'); // express 호출
+  const app = fs() // 아직 뭔지 잘 모르겠음
+
+  app.listen(3000, () => { console.log('3000번 포트 열었음') }) // 3000번 포트를 열 수 있도록 해줌
+
+  /**
+   * get: HTTP 메소드
+   * '/': 라우팅
+   * (req, res) => {}: 콜백 함수
+   */
+  app.get('/', (req, res) => { // '/' 은 보통 home 을 가리킨다. home 에 요청(req)과 응답(res)을 가지고 서버와 통신할 수 있도록 한다.
+    /**
+     * sendFile(): 파일을 호출한다.
+     * __dirname: 파일 이름
+     * '/index.html/': 파일의 상대경로
+     */
+    res.sendFile(__dirname + '/index.html')
+  })
+}
--- server.js
+++ server.js
@@ -5,14 +5,14 @@ app.listen(8080, function () { // 8080 포트로 서버를 열어달라는 뜻
   console.log('listening on 8080')
 });
 
-app.get('/pet', function (req, rep) {
-  rep.send('펫용품 쇼핑할 수 있는 페이지입니다.');
+app.get('/pet', function (req, res) {
+  res.send('펫용품 쇼핑할 수 있는 페이지입니다.');
 });
 
-app.get('/beauty', (req, rep) => {
-  rep.send('뷰티용품 쇼핑할 수 있는 페이지입니다.');
+app.get('/beauty', (req, res) => {
+  res.send('뷰티용품 쇼핑할 수 있는 페이지입니다.');
 });
 
-app.get('/', (req, rep) => {
-  rep.sendFile(__dirname + '/index.html')
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
 });
