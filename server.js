71b0610adc86c0805bee17dd6e9fb67f4fea2784
websteak <ssk0080@gmail.com>
2023-01-07 09:29:47 +0900

    html 에서 통신하기

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+// import { sound } from "./sound.json";
+
 const express = require('express');
+const cors = require('cors')
 const app = express();
+
+app.use(cors())
 
 app.listen(8080, function () { // 8080 포트로 서버를 열어달라는 뜻
   console.log('listening on 8080')
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
 });
 
 app.get('/pet', function (req, res) {
@@ -13,6 +22,24 @@ app.get('/beauty', (req, res) => {
   res.send('뷰티용품 쇼핑할 수 있는 페이지입니다.');
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-});
+app.get('/dog', (req, res) => { // 객체를 전달할 수도 있다.
+  res.json({ sound: '멍멍!!' })
+})
+
+app.get('/user/:id', (req, res) => {
+  // const q = req.params
+  const q = req.query
+  console.log(q)
+  res.send(q.firstname)
+})
+
+const sound = {
+  dog: '멍멍',
+  cat: '야옹',
+}
+
+app.get('/sound/:name', (req, res) => {
+  q = req.params
+  console.log(q)
+  res.send(q)
+})
