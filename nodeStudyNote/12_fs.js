7dbde8eda7bbef1f24f9b126ad2f13e9276ea0eb
websteak <ssk0080@gmail.com>
2023-01-08 21:15:55 +0900

    12. promises 사용한 파일 조작

--- nodeStudyNote/12_fs.js
+++ nodeStudyNote/12_fs.js
@@ -17,4 +17,21 @@ function 파일_이름_변경하기() {
     .then((e) => console.log(e))
     .catch(error => console.error(error))
 }
-파일_이름_변경하기()
+// 파일_이름_변경하기()
+
+// -------------------------------------
+
+function promises__사용한_파일_조작() { // 비동기라서 순서가 정확하지 않을 수 있다.
+  const fs = require('fs').promises;
+
+  fs.readFile('./12_module.txt', 'utf8')
+    .then((data) => console.log(data))
+    .catch(console.error);
+
+  fs.writeFile('./12_module.txt', 'hello, websteak!! hi~!')
+    .catch(console.error)
+
+  fs.appendFile('./12_module.txt', '\nhello, websteak!!!')
+    .catch(console.error)
+}
+promises__사용한_파일_조작();
