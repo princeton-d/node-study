de7432065a1cdec06c21cb4e654e97eb5329fe6e
websteak <ssk0080@gmail.com>
2023-01-17 23:42:53 +0900

    project start

--- express/app.js
+++ express/app.js
@@ -1,7 +1,32 @@
 import express from 'express';
 const app = express();
 
-app.get('/web/:id', (req, res, next) => {
-  res.status(201).json({ name: 'donghyun', firstName: 'kim', id: req.params.id })
+// app.get('/web/:id', (req, res, next) => {
+//   res.status(201).json({ name: 'donghyun', firstName: 'kim', id: req.params.id })
+// });
+
+app.get(
+  '/',
+  (req, res, next) => {
+    console.log('first');
+    next(new Error('error'));
+  },
+  (req, res, next) => {
+    console.log('first2');
+    next();
+  }
+)
+
+app.get('/', (req, res, next) => {
+  console.log('second');
 });
+
+app.use((req, res, next) => {
+  res.status(404).send('Not available!@_@')
+})
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.status(500).send('Sorry, try later!');
+})
 app.listen(8080)
